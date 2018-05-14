5af7b5bd9cd320788cafa0e6eb9a82e52d23d43d
Oleg Ligay <ligayoleg@gmail.com>
2018-05-14 11:30:45 -0400

    final_project
    Updated servo control

--- class_notes/class_wk14_05_07_18_final project/arduino_final_project_beanboozled/arduino_final_project_beanboozled.ino
+++ class_notes/class_wk14_05_07_18_final project/arduino_final_project_beanboozled/arduino_final_project_beanboozled.ino
@@ -1,36 +1,78 @@
 #include <Servo.h>
+ 
+Servo servoX;  // create servo object to control an X axis servo
+Servo servoY; // create servo object to control an Y axis servo
+Servo servo; // create servo object to control a claw servo
 
-Servo servoX;
-Servo servoY;
-Servo servoClaw;
+int servoXPin = 5;  // what pins are our servos connected to?
+int servoYPin = 4;
+int servoClaw = 3;
+int analogXPin = A0;
+int analogYPin = A1;
+int anXRead = 0;
+int anYRead = 0;
 
-int servoXPin = 5;
-int servoYPin = 7;
-int servoClawPin = 6;
-
-int posX = 0;
+int posX = 0;    // variable to store the servo position
 int posY = 0;
+int posClaw = 0;
 
-// Arduino pin numbers
- int SW_pin = 2; // digital pin connected to switch output
- int X_pin = A1; // analog pin connected to X output
- int Y_pin = A0; // analog pin connected to Y output
+int previousX = 0;
+int previousY = 0;
 
+//Button
+int buttonPin = 8;
+int counter = 0;
 void setup() {
-  pinMode(SW_pin, INPUT);
-  digitalWrite(SW_pin, HIGH);
-  Serial.begin(9600);
-}
+  Serial.begin(9600);  // we're gonna use serial comm.
+ 
+  // "attach" servos
+  servoX.attach(servoXPin);  // attaches the servo on pin 11 to the servo object
+  servoY.attach(servoYPin);
+  servo.attach(servoClaw);
 
+  pinMode(buttonPin, INPUT);
+}
+ 
 void loop() {
-  Serial.print("Switch:  ");
-  //Serial.print(digitalRead(SW_pin));
-  Serial.print("\n");
-  Serial.print("X-axis: ");
-  Serial.print(analogRead(A1));
-  Serial.print("\n");
-  Serial.print("Y-axis: ");
-  Serial.println(analogRead(A0));
-  Serial.print("\n\n");
-  delay(500);
+  // see if there is available serial data
+  anXRead = analogRead(analogXPin);
+  anYRead = analogRead(analogYPin);
+  int mapAnXRead = map(anXRead, 0, 1023, 140, 40);
+  int mapAnYRead = map(anYRead, 0, 1023, 180, 90);
+
+  int smoothX = (mapAnXRead * 0.1) + (previousX * 0.9);
+  int smoothY = (mapAnYRead * 0.1) + (previousY * 0.9);
+  previousX = smoothX;
+  previousY = smoothY;
+
+  //Button
+  int buttonState;
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == LOW){
+    counter++;
+    delay(150);  
+  }
+  
+  if(counter == 0)
+    servo.write (0);  // zero degrees
+  if(counter == 1)
+    servo.write(180);
+  //else reset the counter to 0 which resets thr servo to 0 degrees
+  else
+   counter = 0;
+
+  
+  
+  //Serail print
+  Serial.print("X: ");
+  Serial.println(anXRead);
+  Serial.print("Y: ");
+  Serial.println(anYRead);
+  Serial.print("Counter: ");
+  Serial.println(counter);
+ 
+  // "write" for a servo motor just tells it what angle to go to
+  servoX.write(smoothX);
+  servoY.write(smoothY);
+  delay(15);
 }
